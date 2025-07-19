12fad348beefcdba3c2a3f0e1f7963826bdef983
mouad-sellak <mouaadsellak123@gmail.com>
2025-07-19 21:27:49 +0200

    [F09] chore(router): add /admin routes & provider

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,15 +1,35 @@
+// router.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProTipPage from './pages/ProTipPage';
 import HomePage from './pages/HomePage';
+import ProTipPage from './pages/ProTipPage';
+import { AdminAuthProvider } from './context/AdminAuthContext';
+import AdminLoginPage from './pages/admin/AdminLoginPage';
+import AdminProsPage from './pages/admin/AdminProsPage';
+import AdminProtectedRoute from './components/admin/AdminProtectedRoute';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/:slug" element={<ProTipPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<div className="text-center p-8 text-gray-600">Page</div>} />
-      </Routes>
+      <AdminAuthProvider>
+        <Routes>
+          {/* Public */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:slug" element={<ProTipPage />} />
+
+          {/* Admin */}
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+          <Route
+            path="/admin/pros"
+            element={
+              <AdminProtectedRoute>
+                <AdminProsPage />
+              </AdminProtectedRoute>
+            }
+          />
+          {/* Fallback */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </AdminAuthProvider>
     </BrowserRouter>
   );
 }
